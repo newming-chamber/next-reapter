778f60fb4653d7857753db2df3e31696c8b31ed9
Chamber <chamber@newming.co.kr>
2024-05-14 13:24:32 +0900

    sh path edit

--- repeater.sh
+++ repeater.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Start Crontab : $(date '+%Y-%m-%d %H:%M:%S')" >> /home/hankookilbo/next-repeater/cron.log
-output=$(/usr/bin/python3 /home/hankookilbo/next-repeater/repeater.py)
-echo "Result $output" >> /home/hankookilbo/next-repeater/cron.log
+echo "Start Crontab : $(date '+%Y-%m-%d %H:%M:%S')" >> ~/next-repeater/cron.log
+output=$(/usr/bin/python3 ~/next-repeater/repeater.py)
+echo "Result $output" >> ~/next-repeater/cron.log
