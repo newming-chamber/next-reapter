11cf71cf810e32cb9c5c3ab9a142c0a2d0a18210
Chamber <chamber@newming.co.kr>
2024-05-14 13:14:37 +0900

    script edit

--- repeater.sh
+++ repeater.sh
@@ -1,3 +1,5 @@
-#/bin/bash
+#!/bin/bash
 
-/usr/bin/python3 repeater.py
+echo "Start Crontab : $(date '+%Y-%m-%d %H:%M:%S')" >> /home/hankookilbo/next-repeater/cron.log
+output=$(/usr/bin/python3 /home/hankookilbo/next-repeater/repeater.py)
+echo "Result $output" >> /home/hankookilbo/next-repeater/cron.log
