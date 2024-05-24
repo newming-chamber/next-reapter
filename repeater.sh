5130d1504bbed72f419b87a4cdcf286b2b09492a
Chamber <chamber@newming.co.kr>
2024-05-24 09:42:36 +0900

    script end log add

--- repeater.sh
+++ repeater.sh
@@ -3,3 +3,5 @@
 echo "Start Crontab : $(date '+%Y-%m-%d %H:%M:%S')" >> ~/next-repeater/cron.log
 output=$(/usr/bin/python3 ~/next-repeater/repeater.py)
 echo "Result $output" >> ~/next-repeater/cron.log
+echo "END Crontab : $(date '+%Y-%m-%d %H:%M:%S')" >> ~/next-repeater/cron.log
+
