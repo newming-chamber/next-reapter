c0b845a9df38ea557c84a22f87e07ea0e6704300
Chamber <chamber@newming.co.kr>
2024-05-24 15:54:53 +0900

    Upload thresh hold delete

--- module/file_utils.py
+++ module/file_utils.py
@@ -65,15 +65,15 @@ class FileManager:
 
                 copy2(source_path, destination_path)
                 self.logger.info(f"COPY process path {filename}")
-                file_mod_time = self.get_file_mod_time(source_path)
+                # file_mod_time = self.get_file_mod_time(source_path)
 
                 # is_procceed = os.path.exists(
                 #     os.path.join(self.directory_path, "origin_files", filename)
                 # )
-                if file_mod_time > UPLOAD_THRESS_HOLD:
-                    self.parsing_news("prod", filename, destination_path)
-                    self.parsing_news("stage", filename, destination_path)
-                    upload_list.append(filename)
+                # if file_mod_time > UPLOAD_THRESS_HOLD:
+                self.parsing_news("prod", filename, destination_path)
+                self.parsing_news("stage", filename, destination_path)
+                upload_list.append(filename)
 
                 os.remove(destination_path)
                 self.logger.info(f"DELETE process path {destination_path}")
